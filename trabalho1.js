b05e456e0065800df879cfa793c48176f65f18d4
eduardotcarneiro <eduardo.t.carneiro@hotmail.com>
2016-06-21 23:42:41 -0300

    Update trabalho1.js

--- trabalho1.js
+++ trabalho1.js
@@ -177,20 +177,20 @@ var canvas, ctx, velocidade = 9, dist = 0, record, img, frames = 0, startx = 0, 
 	}
 
         function touchHandler(event) {
-  // Get a reference to our coordinates div
+  
   var coordx = event.touches[0].pageX;
   var coordy = event.touches[0].pageY;
 
   if (estadoAtual == estados.jogar && coordx >= 0 && coordy >=0) {
 	estadoAtual = estados.jogando;
 	frames = 0;
-  } else if (estadoAtual == estados.perdeu) {
+  } else if (estadoAtual == estados.perdeu && coordx >= 0 && coordy >=0) {
 	estadoAtual = estados.jogar;
 	obstaculos.limpa();
 	bloco.reset();
   }
 }
-	
+	/*
 	function clique(event) {
 			if (estadoAtual == estados.jogar) {
 				estadoAtual = estados.jogando;
@@ -202,7 +202,7 @@ var canvas, ctx, velocidade = 9, dist = 0, record, img, frames = 0, startx = 0, 
 				obstaculos.limpa();
 				bloco.reset();
 			}
-	};
+	};*/
 	
 	  function esquerda () {
 	    bloco.x -= LARGURA/10;
